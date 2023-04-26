218f1879f93731781d185d05c70208adb19ee7f7
Milco Numan <milco.numan@gmail.com>
2023-04-26 20:39:57 +0200

    All property tests passing

--- week-4/quickcheck/src/main/scala/quickcheck/HeapProperties.scala
+++ week-4/quickcheck/src/main/scala/quickcheck/HeapProperties.scala
@@ -23,9 +23,9 @@ trait HeapProperties(val heapInterface: HeapInterface):
     "delete minumum of heap of one element should return an empty heap" ->
     forAll { (x: Int) =>
       // create a heap with exactly one element, `x`
-      val heap1: List[Node] = ???
+      val heap1: List[Node] = insert(x, empty)
       // delete the minimal element from it
-      val heap0: List[Node] = ???
+      val heap0: List[Node] = deleteMin(heap1)
       // check that heap0 is empty
       isEmpty(heap0)
     }
@@ -35,9 +35,9 @@ trait HeapProperties(val heapInterface: HeapInterface):
     forAll(generatedHeap.suchThat(heap => !isEmpty(heap))) { (heap: List[Node]) =>
       // find the miniminal element of the heap
       // (you don’t need to handle the case of empty heaps because it has been excluded from the heap generator)
-      val min: Int = ???
+      val min: Int = findMin(heap)
       // insert the minimal element to the heap
-      val updatedHeap: List[Node] = ???
+      val updatedHeap: List[Node] = insert(min, heap)
       // find the minimal element of the updated heap should return the same minimal element
       findMin(updatedHeap) == min
     }
@@ -51,15 +51,15 @@ trait HeapProperties(val heapInterface: HeapInterface):
         true
       else
         // find the minimal element
-        val x1: Int = ???
+        val x1: Int = findMin(heap)
         // delete the minimal element of `heap`
-        val heap2: List[Node] = ???
+        val heap2: List[Node] = deleteMin(heap)
         // find the minimal element in `heap2`
-        val x2: Int = ???
+        val x2: Int = findMin(heap2)
         // check that the deleted element is less than or equal to the
         // minimal element of the remaining heap, and that the remaining
         // heap verifies the same property (by recursively calling `check`)
-        val checked: Boolean = ???
+        val checked: Boolean = x2 >= x1 && check(heap2)
         checked
     // check arbitrary heaps
     "continually finding and deleting the minimal element of a heap should return a sorted sequence" ->
@@ -76,16 +76,16 @@ trait HeapProperties(val heapInterface: HeapInterface):
       // - the second also has two duplicate elements insterted, where both are equal
       //   to the lowest value among `x` and `y`
       // finally, meld both heaps.
-      val meldedHeap: List[Node] = ???
+      val meldedHeap: List[Node] = meld(insert(x, insert(x, empty)), insert(y, insert(y, empty)))
       // check that deleting the minimal element twice in a row from the melded heap,
       // and then finding the minimal element in the resulting heap returns the
       // highest value
       val deleteTwoMinAndFindMin: Boolean =
-        ???
+        findMin(deleteMin(deleteMin(meldedHeap))) == Math.max(x,y)
       // check that inserting the lowest value to the melded heap, and then
       // finding the minimal element returns the lowest value
       val insertMinAndFindMin: Boolean =
-        ???
+        findMin(insert(Math.min(x,y), meldedHeap)) == Math.min(x,y)
       // check that both conditions are fulfilled
       deleteTwoMinAndFindMin && insertMinAndFindMin
     }
@@ -115,9 +115,27 @@ trait HeapProperties(val heapInterface: HeapInterface):
   //  4. all the other cases (which should not happen in correct heap
   //     implementations).
   val meldingHeaps: (String, Prop) =
+    def check(m: List[Node], h1: List[Node], h2: List[Node]): Boolean =
+      // 1. the melded heap is empty
+      if m.isEmpty then
+        h1.isEmpty && h2.isEmpty // just 1 line, very short answer
+      else 
+        val minMeld = findMin(m)
+        //  2. the minimum of the melded heap is the minimum of the first source heap
+        if !h1.isEmpty && minMeld == findMin(h1) then
+          // (then, check that after...) just 1 line, single expression. Use recursion!
+          check(deleteMin(m), deleteMin(h1), h2)
+        else
+          if !h2.isEmpty && minMeld == findMin(h2) then  
+            //  3. the minimum of the melded heap is the minimum of the second source heap
+            // (almost the same as 2, should be just 2 lines, copy/paste/edit)
+            check(deleteMin(m), h1, deleteMin(h2))
+          else 
+            //  4. all the other cases (which should not happen)
+            false
     "finding the minimum of melding any two heaps should return the minimum of one or the other of the source heaps" ->
     forAll { (heap1: List[Node], heap2: List[Node]) =>
-      ???
+      check(meld(heap1, heap2), heap1, heap2)
     }
 
   // Random heap generator (used by Scalacheck)
